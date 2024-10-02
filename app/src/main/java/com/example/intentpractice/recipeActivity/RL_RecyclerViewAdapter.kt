dc3efccc1342e31ee4bcc41b5314d5d8412c485d
andretini <andrealijaramosagostini@gmail.com>
2024-10-02 19:08:13 -0300

    Recycler View Start

---
+++ app/src/main/java/com/example/intentpractice/recipeActivity/RL_RecyclerViewAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.intentpractice.recipeActivity
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class RL_RecyclerViewAdapter : RecyclerView.Adapter<RL_RecyclerViewAdapter.MyViewHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): RL_RecyclerViewAdapter.MyViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RL_RecyclerViewAdapter.MyViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+}
